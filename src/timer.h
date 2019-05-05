622ff4bd7afd9d9cd714c3eb0fa7337fb52b49bd
Liwei Cai <cai.lw123@gmail.com>
2019-05-05 01:18:52 -0400

    Rename timer_t to avoid name conflict on Linux

--- src/timer.h
+++ src/timer.h
@@ -17,11 +17,11 @@ typedef enum {
     TMR_DROPOUT_FW,
     TMR_DROPOUT_BW,
     __NUM_TMR
-} timer_t;
+} timer_instance;
 
-void timer_start(timer_t t);
-float timer_stop(timer_t t);
-float timer_average(timer_t t);
+void timer_start(timer_instance t);
+float timer_stop(timer_instance t);
+float timer_average(timer_instance t);
 
 #define PRINT_TIMER_AVERAGE(T) printf(#T " average time: %.3fms\n", timer_average(T) * 1000)
 
